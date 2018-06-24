362dd00793c3b878776b9771ebc5e93ec2f1609e
Grace Shih <vocalise@gmail.com>
2018-06-24 06:41:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,4 +5,16 @@ class App < Sinatra::Base
   get '/' do
     "Hello, World!"
   end
+  
+  get '/name' do
+    "My name is __"
+  end
+  
+  get '/hometown' do
+    "My hometown is __"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is __"
+  end
 end
